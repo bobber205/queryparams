a86adfd678299a7de16a1382bd136d3e6fa1794e
Alex Wait <bobber205@gmail.com>
2014-10-31 09:51:44 -0700

    Updated to properly encode arrays.

--- lib/queryparams.rb
+++ lib/queryparams.rb
@@ -1,21 +1,21 @@
-require "queryparams/version"
+require "queryparams/version"                                                                                                                                                    
 require "cgi"
 
 module QueryParams
 
-  def self.encode(value, key = nil)
-    case value
-    when Hash  then value.map { |k,v| encode(v, append_key(key,k)) }.join('&')
-    when Array then value.map { |v| encode(v, "#{key}[]") }.join('&')
-    when nil   then ''
-    else            
-      "#{key}=#{CGI.escape(value.to_s)}" 
-    end
-  end
+ def self.encode(value, key = nil)
+   case value
+   when Hash  then value.map { |k,v| encode(v, append_key(key,k)) }.join('&')
+   when Array then value.map.with_index { |v, index| encode(v, "#{key}[#{index}]") }.join('&')
+   when nil   then ''
+   else
+     "#{key}=#{CGI.escape(value.to_s)}"
+   end
+ end
 
-  private
+ private
 
-  def self.append_key(root_key, key)
-    root_key.nil? ? key : "#{root_key}[#{key.to_s}]"
-  end
+ def self.append_key(root_key, key)
+   root_key.nil? ? key : "#{root_key}[#{key.to_s}]"
+ end
 end
